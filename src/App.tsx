0e454466bd3d51d0db573905058b71756f031a3d
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-05-14 20:01:12 +0800

    feat: wire up app layout with sidebar, table, and summary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Sidebar from "./components/sidebar/Sidebar"
+import ScheduleTable from "./components/table/ScheduleTable"
+import StatsBar from "./components/summary/StatsBar"
+import ConflictList from "./components/summary/ConflictList"
+
 export default function App() {
     return (
-        <div>
-            hi world
+        <div className="flex h-screen">
+            <Sidebar />
+            <div className="flex flex-col flex-1 overflow-hidden p-4 gap-3">
+                <StatsBar />
+                <ConflictList />
+                <ScheduleTable />
+            </div>
         </div>
     )
 }
