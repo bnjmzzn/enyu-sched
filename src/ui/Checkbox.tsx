8e9423765d2092ca2ae729410e96becc47cdf5b7
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-05-18 11:05:05 +0800

    style: make checkboxes bigger

--- src/ui/Checkbox.tsx
+++ src/ui/Checkbox.tsx
@@ -19,6 +19,7 @@ export default function Checkbox({ checked, indeterminate = false, onChange }: P
             type="checkbox"
             checked={checked}
             onChange={onChange}
+            className="size-4"
         />
     )
 }
